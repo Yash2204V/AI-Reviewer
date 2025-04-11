a109b37c10d5c6065ae3c080125d4a0f7a3d2f27
YASH2204V <yash.v220104@gmail.com>
2025-04-11 17:16:21 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const app = require("./src/app");
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, ()=>{
-    console.log("Server running on http://localhost:3000");
+app.listen(PORT, ()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
 })
